6afdfee641300c6a2147f89d268619e275f302f4
JJ Garlitos <acoustic_ninja@hotmail.com>
2019-08-07 18:41:26 -0600

    Added books and created Library class

--- App.cs
+++ App.cs
@@ -8,15 +8,18 @@ namespace ConsoleLibrary
         public void Setup()
         {
             //TODO create instances of some books
-            //TODO add those books to your library
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+            Book book2 = new Book("Dune", "Frank Herbert", 500);
+            Book book3 = new Book("Patsy", "Nicole Benn-Dennis", 250);
+            //TODO add those books to your library            
         }
 
         public void Run()
         {
             //TODO write our application
-                //1. we need a greeting and menu navigation
-                //2. must be able to view, checkout, and return books
-                    //[STRETCH GOAL] allow the user to create instances of books 
+            //1. we need a greeting and menu navigation
+            //2. must be able to view, checkout, and return books
+            //[STRETCH GOAL] allow the user to create instances of books 
         }
 
         // NOTE Our constructor method runs when an instance of an App is created and the logic within assigns a new instance of a Library to the property Library on our app
--- Models/Library.cs
+++ Models/Library.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
+
 namespace ConsoleLibrary.Models
 {
     public class Library
     {
         //TODO Create the properties and methods that this class will need
-
+        public List<Book> Books { get; set; }
     }
 }
